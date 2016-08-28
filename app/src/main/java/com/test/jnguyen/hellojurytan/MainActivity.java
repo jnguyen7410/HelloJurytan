8bad7142e92d63ee9fe742e59af0fa6260818973
jnguyen7410 <jnguyen7410@gmail.com>
2016-08-28 12:14:30 -0700

    Made some changes to the main activity

--- app/src/main/java/com/test/jnguyen/hellojurytan/MainActivity.java
+++ app/src/main/java/com/test/jnguyen/hellojurytan/MainActivity.java
@@ -1,18 +1,37 @@
 package com.test.jnguyen.hellojurytan;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public final static String EXTRA_MESSAGE = "com.jnguyen.hellojurytan.MESSAGE";
+    TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     public void sendMessage(View view) {
